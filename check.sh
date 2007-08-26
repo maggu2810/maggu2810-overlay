f78967f0086680b76c5227860d852bb81a839130
Oliver Hoffmann <maggu2810@web.de>
2007-08-26 21:45:06 +0000

    check.sh can correct errors now

--- check.sh
+++ check.sh
@@ -4,21 +4,36 @@ eouc=`grep -n "END-OF-USER-CONFIGURATION" proto.ebuild | cut -d ":" -f 1`
 
 for pkg in `find -mindepth 2 -maxdepth 2 -type d -not -name "CVS" -printf "%P\n"`; do
 	if grep -q "END-OF-USER-CONFIGURATION" $pkg/*.ebuild 2>/dev/null; then
-		first=""
+		first=`ls $pkg/*.ebuild`
+		first=${first%%.ebuild*}.ebuild
+		lastline=`diff -n proto.ebuild $first | grep "^[ad]" | tail -n1 | sed "s:[ad]\([0-9]*\).*:\1:"`
+		if [ $lastline -ge $[ $eouc -3 ] ]; then
+			line=`grep -n "END-OF-USER-CONFIGURATION" $first | cut -d ":" -f 1`
+			tail -n $[ `cat proto.ebuild | wc -l` - $eouc - 1 ] proto.ebuild > /tmp/pck1.tmp
+			tail -n $[ `cat $first | wc -l` - $line - 1 ] $first > /tmp/pck2.tmp
+			diff -u /tmp/pck[12].tmp | colordiff
+			echo "Error in $pkg: ebuild has modified lines after END-OF-USER-CONFIGURATION"
+			echo -n "Correct (y/N)? "
+			read asw
+			if [ "$asw" = "y" ]; then
+				line=`grep -n "END-OF-USER-CONFIGURATION" $first | cut -d ":" -f 1`
+				head -n $[ $line + 1 ] $first > /tmp/pck.tmp
+				tail -n $[ `cat proto.ebuild | wc -l` - $eouc - 1 ] proto.ebuild >> /tmp/pck.tmp
+				mv /tmp/pck.tmp $first
+			fi
+		fi
 		for ebuild in $pkg/*.ebuild; do
-			if [ ${#first} -eq 0 ]; then
-				first=$ebuild
-			else
+			if [ $first != $ebuild ]; then
 				if ! diff -q $first $ebuild >/dev/null; then
-					echo "Error in $pkg: ebuilds differ:"
-					diff -u $first $ebuild
+					diff -u $first $ebuild | colordiff
+					echo "Error in $pkg: ebuilds differ"
+					echo -n "Correct (y/N)? "
+					read asw
+					if [ "$asw" = "y" ]; then
+						cp -f $first $ebuild
+					fi
 				fi
 			fi
 		done
-		lastline=`diff -n proto.ebuild $first | grep "^[ad]" | tail -n1 | sed "s:[ad]\([0-9]*\).*:\1:"`
-		if [ $lastline -ge $[ $eouc - 3 ] ]; then
-			echo "Error in $pkg: ebuild has modified lines after END-OF-USER-CONFIGURATION:"
-			diff -u proto.ebuild $first
-		fi
 	fi
 done
